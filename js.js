e4c6d09e6079f5ba94512c38e427afc1eb377d76
mahmoudadham63 <31295274+mahmoudadham63@users.noreply.github.com>
2021-01-13 00:54:19 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,3 @@
-// document.addEventListener("contextmenu","copy",function(e){
-// e.preventDefault();
-// alert("coped is not allowed here");
-// })
-
-// document.addEventListener("copy", function(e) {
-//     e.preventDefault();
-//     alert("not allowed");
-//     return false;
-
-// })
 function mouseMoveHandler(e) {
     e.preventDefault();
     alert("copied is not allowed here");
